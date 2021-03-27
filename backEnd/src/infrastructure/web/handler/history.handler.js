eca57ea2a0011f2332a56d18d5afcfa1f4896c5d
venturaf <franner.ventura@gmail.com>
2021-03-26 21:31:08 -0300

    [dR-back-v2] mejoras de history y balance
    [dR-back-v2] mejoras de history y balance

--- backEnd/src/infrastructure/web/handler/history.handler.js
+++ backEnd/src/infrastructure/web/handler/history.handler.js
@@ -1,27 +1,12 @@
 const { HISTORY_COLLECTION } = process.env;
-const { OK, INTERNAL_SERVER_ERROR } = require("http-status-codes");
-
 module.exports = (repository) => {
-
-    const { HistoryRepository } = repository;
-    const History = HistoryRepository.model(HISTORY_COLLECTION, HistoryRepository.historySchema);
+    const { HistoryRepository, model } = repository;
+    const History = model(HISTORY_COLLECTION, HistoryRepository.historySchema);
     return {
-        saveHistory: async (req, res) => {
-            if (req.body) req = req.body; 
-
-            const { rut, action, balance, to } = req;
-            let now= new Date();
-            const history = new History({ date: Date(), rut, action, balance, to });
-            await history.save()
-            .then((r) => res.status(OK).json(r))
-            .catch((e) => res.status(INTERNAL_SERVER_ERROR).send(e));
-        },
-        findHistory: async (req, res) => {
-            const { rut } = req.body;
-            await History.find({ rut }).exec()
-                .then((r) => res.status(OK).json(r))
-                .catch((e) => res.status(INTERNAL_SERVER_ERROR).send(e))
-
+        findHistory: async ({ rut }) => {
+            return await History.find({ rut }).exec()
+                .then((r) => r)
+                .catch((e) => console.log(e));
         },
     };
 };
--- backEnd/src/infrastructure/web/router/balance.js
+++ backEnd/src/infrastructure/web/router/balance.js
@@ -1,29 +1,29 @@
 const balanceHandler = require('./../handler/balance.handler');
 const { validBalance } = require('../midleware/balance.midleware');
-const { status } = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 module.exports = (router, repository) => {
   const { currentBalance, depositBalance, withdrawBalance, transferBalance } = balanceHandler(repository);
   router.get('/:rut', async (req, res) => {
     const balance = await currentBalance(req.params);
-    if (balance && balance.rut) res.status(status.OK).json(balance);
-    else res.status(status.NOT_FOUND).json(balance);
+    if (balance && balance.rut) res.status(StatusCodes.OK).json(balance);
+    else res.status(StatusCodes.NOT_FOUND).json(balance);
   });
   router.post('/deposit', validBalance, async (req, res) => {
     const isDosited = await depositBalance(req.body);
-    if (isDosited) res.status(status.OK).json("Cash deposited");
-    else res.status(status.INTERNAL_SERVER_ERROR).json("an error ocurre while depositing cash");
+    if (isDosited) res.status(StatusCodes.OK).json("Cash deposited");
+    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while depositing cash");
   });
   router.post('/withdraw', validBalance, async (req, res) => {
     const isDosited = await withdrawBalance(req.body);
-    if (isDosited && typeof isDosited === 'string') res.status(status.NOT_ACCEPTABLE).json(isDosited);
-    else if (isDosited) res.status(status.OK).json("Cash withdrawed");
-    else res.status(status.INTERNAL_SERVER_ERROR).json("an error ocurre while withdraw cash");
+    if (isDosited && typeof isDosited === 'string') res.status(StatusCodes.NOT_ACCEPTABLE).json(isDosited);
+    else if (isDosited) res.status(StatusCodes.OK).json("Cash withdrawed");
+    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while withdraw cash");
   });
   router.post('/transfer', validBalance, async (req, res) => {
     const isDosited = await transferBalance(req.body);
-    if (isDosited && typeof isDosited === 'string') res.status(status.NOT_ACCEPTABLE).json(isDosited);
-    else if (isDosited) res.status(status.OK).json("Balance have been transfer successfull");
-    else res.status(status.INTERNAL_SERVER_ERROR).json("an error ocurre while transfer cash");
+    if (isDosited && typeof isDosited === 'string') res.status(StatusCodes.NOT_ACCEPTABLE).json(isDosited);
+    else if (isDosited) res.status(StatusCodes.OK).json("Balance have been transfer successfull");
+    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while transfer cash");
   });
   return router;
 }
