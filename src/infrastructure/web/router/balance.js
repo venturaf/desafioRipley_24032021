5a2d22c656aae9030ef69d6872cccca739e0b576
venturaf <franner.ventura@gmail.com>
2021-03-28 11:29:31 -0300

    [dR-back-v2] se cambia la respuesta del deposito
    [dR-back-v2] se cambia la respuesta del deposito

--- src/infrastructure/web/router/balance.js
+++ src/infrastructure/web/router/balance.js
@@ -10,19 +10,19 @@ module.exports = (router, repository) => {
   });
   router.post('/deposit', validBalance, async (req, res) => {
     const isDosited = await depositBalance(req.body);
-    if (isDosited) res.status(StatusCodes.OK).json("Cash deposited");
+    if (isDosited) res.status(StatusCodes.OK).json(isDosited);
     else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while depositing cash");
   });
   router.post('/withdraw', validBalance, async (req, res) => {
     const isDosited = await withdrawBalance(req.body);
     if (isDosited && typeof isDosited === 'string') res.status(StatusCodes.NOT_ACCEPTABLE).json(isDosited);
-    else if (isDosited) res.status(StatusCodes.OK).json("Cash withdrawed");
+    else if (isDosited) res.status(StatusCodes.OK).json(isDosited);
     else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while withdraw cash");
   });
   router.post('/transfer', validBalance, async (req, res) => {
     const isDosited = await transferBalance(req.body);
     if (isDosited && typeof isDosited === 'string') res.status(StatusCodes.NOT_ACCEPTABLE).json(isDosited);
-    else if (isDosited) res.status(StatusCodes.OK).json("Balance have been transfer successfull");
+    else if (isDosited) res.status(StatusCodes.OK).json(isDosited);
     else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while transfer cash");
   });
   return router;
