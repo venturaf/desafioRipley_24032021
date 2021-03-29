36f099ec47026373af2952aa6887e628904e0e9c
venturaf <franner.ventura@gmail.com>
2021-03-29 01:18:03 -0300

    [dr-back-v4] cambio en los mensajes
    [dr-back-v4] cambio en los mensajes

--- src/infrastructure/web/router/balance.js
+++ src/infrastructure/web/router/balance.js
@@ -6,24 +6,24 @@ module.exports = (router, repository) => {
   router.get('/current/:rut', async (req, res) => {
     const balance = await currentBalance(req.params);
     if (balance && balance.rut) res.status(StatusCodes.OK).json(balance);
-    else res.status(StatusCodes.NOT_FOUND).json(balance);
+    else res.status(StatusCodes.OK).json(balance);
   });
   router.post('/deposit', validBalance, async (req, res) => {
     const isDosited = await depositBalance(req.body);
-    if (isDosited) res.status(StatusCodes.OK).json("Cash deposited");
-    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while depositing cash");
+    if (isDosited) res.status(StatusCodes.OK).json("Deposito exitoso");
+    else res.status(StatusCodes.OK).json("Algo a ocurrido mientras hacia el deposito");
   });
   router.post('/withdraw', validBalance, async (req, res) => {
     const isDosited = await withdrawBalance(req.body);
     if (isDosited && typeof isDosited === 'string') res.status(StatusCodes.NOT_ACCEPTABLE).json(isDosited);
-    else if (isDosited) res.status(StatusCodes.OK).json("Cash withdrawed");
-    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while withdraw cash");
+    else if (isDosited) res.status(StatusCodes.OK).json("Retiro exitoso");
+    else res.status(StatusCodes.OK).json("Algo a ocurrido mientras hacia el retiro");
   });
   router.post('/transfer', validBalance, async (req, res) => {
     const isDosited = await transferBalance(req.body);
     if (isDosited && typeof isDosited === 'string') res.status(StatusCodes.NOT_ACCEPTABLE).json(isDosited);
-    else if (isDosited) res.status(StatusCodes.OK).json("Balance have been transfer successfull");
-    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("an error ocurre while transfer cash");
+    else if (isDosited) res.status(StatusCodes.OK).json("Transferencia exitosa");
+    else res.status(StatusCodes.OK).json("Algo a ocurrido mientras hacia la transferencia");
   });
   return router;
 }
